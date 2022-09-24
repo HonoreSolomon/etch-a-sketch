6e18dfa43b922f22c2b4f004fa4c5e15a02eae34
Honore Solomon <honoresolomon@hotmail.com>
2022-09-24 14:39:16 -0700

    debugging color changes

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const container = document.querySelector('.container');
 const size = 16;
+let color = 'black';
+const btns = document.querySelectorAll(
+  '.btns-color button'
+);
 
 const getSize = () => {
   const input = document.querySelector('.btns input');
@@ -15,5 +19,52 @@ const makeRows = (rows, cols) => {
     container.appendChild(cell).className = 'grid-item';
   }
 };
+const rainbow = () =>
+  '#' +
+  ((Math.random() * 0xffffff) << 0)
+    .toString(16)
+    .padStart(6, '0');
+function setColor() {
+  switch (color) {
+    case 'black':
+      for (const div of divs) {
+        div.style.backgroundColor = '#fff';
+      }
+      console.log('black');
+      break;
+    case 'white':
+      for (const div of divs) {
+        div.style.backgroundColor = '#000';
+      }
+      console.log('white');
+      break;
+    case 'Rainbow':
+      for (const div of divs) {
+        div.style.backgroundColor = '#fff';
+        this.style.setProperty('--rainbow', rainbow());
+        console.log('rainbow');
+      }
+      break;
+    case 'reset':
+      return color === ('black' || 'rainbow')
+        ? (div.style.backgroundColor = '#fff')
+        : (div.style.backgroundColor = '#000');
+      console.log('reset');
+  }
+  color = this.value;
+  console.log(color);
+}
 
+function changeColor() {
+  this.classList.add(`${color}`);
+}
 makeRows(size, size);
+const divs = document.querySelectorAll('.grid-item');
+
+for (const btn of btns) {
+  btn.addEventListener('click', setColor);
+}
+
+for (const div of divs) {
+  div.addEventListener('mouseover', changeColor);
+}
